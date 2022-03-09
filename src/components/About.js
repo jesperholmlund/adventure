6b54970f6e25d0faf611e1752bfcb8f41f9bf46a
jesperholmlund <jesper.holmlund@student.kyh.se>
2022-03-09 15:58:16 +0100

    first day

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../dist/css/About.css";
+import { FcAbout } from "react-icons/fc";
+
+const About = () => {
+  return (
+    <div id="about">
+      <div id="aboutImage"></div>
+      <div id="aboutInfo">
+        Om mig Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../dist/css/Nav.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Projektlista</li>
+        <li>Om mig</li>
+        <li>Kontakt</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
